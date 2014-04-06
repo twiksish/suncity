fa4ab9e144266b8c39035f69fd8392ec0d14c22c
Vicky <twiks@Vickys-MacBook-Pro.local>
2014-04-06 22:37:03 +0700

    Added sign up pop up

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
 });
+
+
+/*
+ * Sign in form opening
+ */
+
+$(document).on('ready', function(){
+    $('.header__top__sign-up__handler .button').on('click',function(){
+       $('.header__top__sign-up__form').fadeToggle();
+    });
+});
