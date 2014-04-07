f639d1d46d7bc6df27bef71bdea31579093a921b
Vicky <twiks@Vickys-MacBook-Pro.local>
2014-04-07 19:07:07 +0700

    Added slider to catalog page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ $(document).on('ready', function(){
        $('.header__top__sign-up__form').fadeToggle();
     });
 });
+
+
+$(document).on('ready', function() {
+    $('.slider').bjqs({
+        'height' : 298,
+        'width' : 950,
+        'responsive' : false,
+        'animtype' : 'slide',
+        'nexttext' : '',
+        'prevtext' : '',
+        'centermarkers' : false,
+        'usecaptions' : false
+    });
+});
